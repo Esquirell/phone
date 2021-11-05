607677aa7c494702a27978ad78a5a5802c2848f1
kyksa byksa <kyksa@gmail.com>
2021-11-05 19:16:23 +0200

    Added validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,8 +8,13 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function findUser($id)
+    public function findUser(Request $request)
     {
+        $validated = $request->validate([
+            'id' => 'required|integer'
+        ]);
+
+        $id = $validated['id'];
         $user = User::with('phones')->findOrFail($id);
         $user_name = $user->name;
         $user_birth_date = $user->birth_date;
@@ -18,12 +23,18 @@ class UserController extends Controller
         foreach ($user->phones as $phone) {
             $phones[] = $phone->number;
         }
-
         dd($user_name, $user_birth_date, $phones);
     }
 
-    public function refillBalance($number, $sum)
+    public function refillBalance(Request $request)
     {
+        $validated = $request->validate([
+            'number' => 'required|string|size:13',
+            'sum' => 'required|integer|min:1|max:100'
+        ]);
+
+        $number = $validated['number'];
+        $sum = $validated['sum'];
         if ($sum > 0 && $sum <= 100) {
             $phone = Phone::firstWhere('number', $number);
             $phone->balance += $sum;
