88be8bf4049541b8898ba43d94f36263179b379f
kyksa byksa <kyksa@gmail.com>
2021-11-05 18:48:30 +0200

    Created factory for Phone model

---
+++ database/factories/PhoneFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PhoneFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $code_operators = [50, 67, 63, 68];
+        $key = array_rand($code_operators);
+        return [
+            'number' => '+380'
+                .$code_operators[$key]
+                .$this->faker
+                    ->randomNumber(7,true),
+            'balance' => $this->faker->numberBetween(-5000, 15000)/100
+        ];
+    }
+}
