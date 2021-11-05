1c272a5f78d3cdc605908b3e578b5968a83deea4
kyksa byksa <kyksa@gmail.com>
2021-11-05 18:51:01 +0200

    Added a method to search for a user by ID

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,5 +8,17 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    public function findUser($id)
+    {
+        $user = User::with('phones')->findOrFail($id);
+        $user_name = $user->name;
+        $user_birth_date = $user->birth_date;
+        $phones = [];
 
+        foreach ($user->phones as $phone) {
+            $phones[] = $phone->number;
+        }
+
+        dd($user_name, $user_birth_date, $phones);
+    }
 }
